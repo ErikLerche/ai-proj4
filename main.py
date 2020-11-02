1291acb8f088d62dcc497ad31a96ccd41b15263e
ErikLerche <40499812+ErikLerche@users.noreply.github.com>
2020-11-01 22:13:27 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from sklearn.tree import DecisionTreeClassifier     # https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+from sklearn.ensemble import RandomForestClassifier # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+from sklearn.linear_model import LogisticRegression # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+from sklearn.svm import SVC                         # https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+from sklearn.linear_model import SGDRegressor       # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDRegressor.html
+from sklearn.linear_model import Ridge              # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+
+def main():
+    # runDecisionTree()
+    # runRandomForest()
+    # runLogisticRegression()
+    # runSVM()
+    # runSGDRegressor()
+    # runRidge()
+    # runPrintResults()
+
+
+if __name__ == '__main__':
+    main()
