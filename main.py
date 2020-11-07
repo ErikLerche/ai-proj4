f307434a05cf5b828a4c6b34573b8e59d511f01a
peter <peternguyentn98@gmail.com>
2020-11-07 17:42:17 -0500

    added more code for q2

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score    
 import pandas as pd  
+from sklearn.metrics import mean_squared_error
 
 def main():
     leaftrain = pd.read_csv('leaf/train_data.csv')
@@ -103,26 +104,49 @@ def runSVM(train_x, train_y, validation_x, validation_y, test_x, test_y):
 
 def runSGDRegressor(train_x, train_y, validation_x, validation_y, test_x, test_y):
     clf = make_pipeline(StandardScaler(), SGDRegressor(random_state=1, penalty='l2', n_iter_no_change=5, eta0=0.5, power_t=0.5))
-    scaler = StandardScaler()
-    clf.fit(scaler.transform(train_x), scaler.transform(train_y))
+    
+    clf.fit(train_x, train_y)
 
-    score = clf.score(validation_x, validation_y)
-    score2 = clf.score(test_x, test_y)
+    pred = clf.predict(validation_x)
+    pred2 = clf.predict(test_x)
+
+    mean = mean_squared_error(pred, validation_y, squared=False)
+    mean2 = mean_squared_error(pred2, test_y, squared=False)
+
     print('Results for SGD Regressor')
-    print('valdidation data: ', score) # 0.7624346865676033
-    print('test data: ', score2) # 0.7152749971591288
+    print('valdidation data: ', mean) # 0.7624346865676033
+    print('test data: ', mean2) # 0.7152749971591288
     print('\n')
 
 def runRidge(train_x, train_y, validation_x, validation_y, test_x, test_y):
-    clf = Ridge(random_state=1, solver='cholesky', max_iter=100, normalize=True)
+    # scaling training data
+    scaler = StandardScaler()
+    train_x = scaler.fit_transform(train_x)
+    train_y = train_y.values.reshape(-1,1)
+    train_y = scaler.fit_transform(train_y)
+
+    # scaling validation data
+    validation_x = scaler.fit_transform(validation_x)
+    validation_y = validation_y.values.reshape(-1,1)
+    validation_y = scaler.fit_transform(validation_y)
+
+    # scaling test data
+    test_x = scaler.fit_transform(test_x)
+    test_y = test_y.values.reshape(-1,1)
+    test_y = scaler.fit_transform(test_y)
+
+    clf = Ridge(random_state=1, alpha=7, solver='sag', normalize=True)
 
     clf.fit(train_x, train_y)
 
-    score = clf.score(validation_x, validation_y)
-    score2 = clf.score(test_x, test_y)
+    pred = clf.predict(validation_x)
+    pred2 = clf.predict(test_x)
+
+    mean = mean_squared_error(pred, validation_y, squared=False)
+    mean2 = mean_squared_error(pred2, test_y, squared=False)
     print('Results for Ridge Regression')
-    print('valdidation data: ', score)
-    print('test data: ', score2)
+    print('valdidation data: ', mean)
+    print('test data: ', mean2)
     print('\n')
 
 if __name__ == '__main__':
